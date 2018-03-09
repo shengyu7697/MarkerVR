65a1260612c465758245cff96bc2073be2d8b1f7
shengyu <shengyu7697@gmail.com>
2018-03-09 11:32:39 +0800

    add argv and usage to marker_creator

--- src/MarkerCreator/main.cpp
+++ src/MarkerCreator/main.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -9,24 +10,46 @@ using namespace std;
 using namespace cv;
 using namespace aruco;
 
-void createArUcoMarker()
+void usage(const char *progname)
+{
+    printf("\nThis program is used to create markers\n"
+        "Usage: %s num side_pixels\n", progname);
+}
+
+void createArUcoMarker(int num, int sidePixels)
 {
 	Mat outputImage;
 	Ptr<Dictionary> markerDictionary = getPredefinedDictionary(PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+	char buf[100];
 
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < num; i++)
 	{
-		drawMarker(markerDictionary, i, 100, outputImage, 1);
+		drawMarker(markerDictionary, i, sidePixels, outputImage, 1);
 		ostringstream oss;
 		string prefix = "4x4Marker_";
-		oss << prefix << i << ".png";
+		snprintf(buf, sizeof(buf), "%02d", i);
+		oss << prefix << buf << ".png";
+		cout << oss.str() << endl;
 		imwrite(oss.str(), outputImage);
 	}
 }
 
-int main(void)
+int main(int argc, const char *argv[])
 {
-	createArUcoMarker();
+	int num = 50;
+	int sidePixels = 100;
+
+	if (argc < 3) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	num = atoi(argv[1]);
+	sidePixels = atoi(argv[2]);
+
+	printf("create %d markers, side pixels: %d\n", num, sidePixels);
+	createArUcoMarker(num , sidePixels);
+	printf("done.\n");
 
 	return 0;
 }
